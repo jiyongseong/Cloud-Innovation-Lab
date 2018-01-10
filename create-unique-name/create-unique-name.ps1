85ee984637c60eed9014cf53f692ef2257ea66af
Ji Yong Seong <jyseong@microsoft.com>
2018-01-10 16:49:08 +0900

    create unique name for azure

---
+++ create-unique-name/create-unique-name.ps1
@@ -0,0 +1,6 @@
+﻿$resourceGroupnName = "rg-sqlcluster-krc"
+$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+$utf8 = New-Object -TypeName System.Text.UTF8Encoding
+$hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($resourceGroupnName)))
+
+$hash.ToLower() -replace '-', ''
